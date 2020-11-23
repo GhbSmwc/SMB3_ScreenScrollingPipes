ee12c4c55c12a15ee63f0d839dcbdae1c4fbc7bb
GhbSmwc <GhbSmwc@users.noreply.github.com>
2020-11-23 18:30:56 -0500

    due to changes during moderation, pixi macro labels broke
    Currently, pixi suffers the inability of macrolabels.

--- Pixi/routines/DetectSpriteCarriedInSSP.asm
+++ Pixi/routines/DetectSpriteCarriedInSSP.asm
@@ -3,22 +3,22 @@ incsrc "../SSPDef/Defines.asm"
 ;;Carry: Clear if outside pipe (should be visible), Set if inside pipe (should be invisible). This will determine
 ;;should the sprite being carried turn invisible with the player traveling through SSP.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	?.PlayerPipeStatus
+	PlayerPipeStatus:
 		LDA !Freeram_SSP_PipeDir	;\If player is outside...
 		AND.b #%00001111		;|
-		BEQ ?.Visible			;/
+		BEQ Visible			;/
 		LDA !Freeram_SSP_EntrExtFlg	;\...or is in a pipe, but not in his stem phase
 		CMP #$01			;|
-		BNE ?.Visible			;/
+		BNE Visible			;/
 		LDA !Freeram_SSP_PipeTmr	;\...or in a pipe, entering, but before Mario turns invisible.
-		BNE ?.Visible			;/
-	?.SpriteCarried
+		BNE Visible			;/
+	SpriteCarried:
 		LDA !14C8,x			;\...or if sprite not carried
 		CMP #$0B			;|then make sprite visible.
-		BNE ?.Visible			;/
-	?.Invisible
+		BNE Visible			;/
+	Invisible:
 		SEC
 		RTL
-	?.Visible
+	Visible:
 		CLC
 		RTL
