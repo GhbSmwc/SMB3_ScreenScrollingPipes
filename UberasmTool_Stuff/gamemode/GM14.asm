21bec27f3d5f5ac8373a4f9e923351fabfc4e248
GhbSmwc <GhbSmwc@users.noreply.github.com>
2021-03-13 22:40:30 -0500

    info added
    Someone reported issues with the titlescreen. The pipes don't work.
    
    This is an oversight that I didn't expect or have forgotten that these pipes may be used in levels with a gamemode other than 14 (titlescreen gamemode is not 14, but 7): https://www.smwcentral.net/?p=viewthread&t=105416

--- UberasmTool_Stuff/gamemode/GM14.asm
+++ UberasmTool_Stuff/gamemode/GM14.asm
@@ -1,4 +1,7 @@
-;gamemode 14.
+;gamemode 14 (level).
 main:
+	;You can have codes (including JSL to library codes, just like the one below here) between the main label (above) and the code below here, if you want
+	;third-party stuff here.
 	JSL ScreenScrollingPipes_SSPMaincode
+	;...or codes here before the RTL.
 	RTL
