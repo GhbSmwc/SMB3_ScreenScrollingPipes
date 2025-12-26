4efe884f2029dc621b8174a437cf391489b006af
? <GhbSmwc@users.noreply.github.com>
2025-12-26 14:41:30 -0500

    ...

--- SSPDef/Defines.asm
+++ SSPDef/Defines.asm
@@ -189,6 +189,14 @@ endif
   ;^0 = only using vanilla sprites in your entire hack
   ; 1 = using custom sprites.
   ; This is only used during setting the player's facing direction upon exiting horizontal pipe caps while riding yoshi.
+ !Setting_SSP_SetXYFractionBits = 1
+  ;^0 = no
+  ; 1 = yes (this writes to the factional components of the player's XY position used for RAM $7B and $7D's speed to change position, RAM $13DA and
+  ;     $13DC). With this option, you'll have consistent positioning (rather than sometimes 1 pixel off) for things like exiting pipes.
+ !Setting_SSP_XPositionFractionSetTo = $00
+  ;^Fractional amount to set $13DA. It's format is $X0, where X is a value from 0 to F, representing X/16 fraction. Not used if !Setting_SSP_SetXYFractionBits == 0
+ !Setting_SSP_YPositionFractionSetTo = $00
+  ;^Same as above but for $13DC.
 ;Pipe travel speeds:
 ;Use only values $01-$7F (negative speeds already calculated).
  if !Setting_SSP_FuSoYaSpd == 0		;>Don't change this if statement.
