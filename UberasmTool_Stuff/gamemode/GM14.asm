771576f48716a2e44d0102e5d85bb59bc3fec7d9
? <GhbSmwc@users.noreply.github.com>
2025-12-30 21:23:51 -0500

    WIP
    -Some file renaming
    -Added a much better way to migrate map16 tiles to a different page

--- UberasmTool_Stuff/gamemode/GM14.asm
+++
@@ -1,7 +0,0 @@
-;gamemode 14 (level).
-main:
-	;You can have codes (including JSL to library codes, just like the one below here) between the main label (above) and the code below here, if you want
-	;third-party stuff here.
-	JSL ScreenScrollingPipes_SSPMaincode
-	;...or codes here before the RTL.
-	RTL
